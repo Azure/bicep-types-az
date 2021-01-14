760782104a02385543f7601a2dfe770d5ca9f24d
Anthony Martin <antmarti@microsoft.com>
2021-01-13 16:44:14 -0800

    Index resource types by scope (#78)

--- src/Bicep.Types.Az.UnitTests/TypeLoaderTests.cs
+++ src/Bicep.Types.Az.UnitTests/TypeLoaderTests.cs
@@ -12,7 +12,29 @@ namespace Azure.Bicep.Types.Az.UnitTests
         public void TypeLoader_can_load_all_types_without_throwing()
         {
             var typeLoader = new TypeLoader();
-            foreach (var kvp in typeLoader.ListAllAvailableTypes())
+            var indexedTypes = typeLoader.GetIndexedTypes();
+
+            foreach (var kvp in indexedTypes.Tenant)
+            {
+                var resourceType = typeLoader.LoadResourceType(kvp.Value);
+            }
+
+            foreach (var kvp in indexedTypes.ManagementGroup)
+            {
+                var resourceType = typeLoader.LoadResourceType(kvp.Value);
+            }
+
+            foreach (var kvp in indexedTypes.Subscription)
+            {
+                var resourceType = typeLoader.LoadResourceType(kvp.Value);
+            }
+
+            foreach (var kvp in indexedTypes.ResourceGroup)
+            {
+                var resourceType = typeLoader.LoadResourceType(kvp.Value);
+            }
+
+            foreach (var kvp in indexedTypes.Extension)
             {
                 var resourceType = typeLoader.LoadResourceType(kvp.Value);
             }
