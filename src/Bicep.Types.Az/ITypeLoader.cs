81e019640c882568953c0aa025014605d66141dd
Anthony Martin <antmarti@microsoft.com>
2021-02-04 21:28:03 -0500

    Revert "Index resource types by scope (#78)" (#90)
    This reverts commit 760782104a02385543f7601a2dfe770d5ca9f24d.

--- src/Bicep.Types.Az/ITypeLoader.cs
+++ src/Bicep.Types.Az/ITypeLoader.cs
@@ -12,6 +12,6 @@ namespace Azure.Bicep.Types.Az
 
         ResourceType LoadResourceType(TypeLocation location);
 
-        IndexedTypes GetIndexedTypes();
+        TypeIndex GetIndexedTypes();
     }
 }
