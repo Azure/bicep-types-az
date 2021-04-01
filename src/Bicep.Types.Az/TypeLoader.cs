f8792c81124b152d8d94f0d7579dce6fa5cc9c0f
Anthony Martin <antmarti@microsoft.com>
2021-04-01 15:02:51 -0400

    Compress bicep types into a zip archive (#160)
    * csproj changes to compress and embed types
    
    * Read from embedded zip file
    
    * Declare target inputs/outputs
    
    * Remove unused ITypeLoader method

--- src/Bicep.Types.Az/TypeLoader.cs
+++ src/Bicep.Types.Az/TypeLoader.cs
@@ -3,6 +3,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using Azure.Bicep.Types.Az.Index;
 using Azure.Bicep.Types.Concrete;
@@ -11,69 +12,63 @@ namespace Azure.Bicep.Types.Az
 {
     public class TypeLoader : ITypeLoader
     {
+        private readonly ZipArchive zipArchive;
+
+        public TypeLoader()
+        {
+            var zipArchiveStream = typeof(TypeLoader).Assembly.GetManifestResourceStream("generated_types.zip");
+            this.zipArchive = new ZipArchive(zipArchiveStream);
+        }
+
         private const string TypeContainerName = "types.json";
         private const string TypeIndexResourceName = "index.json";
 
         private static string GetTypeContainerResourceName(string providerNamespace, string apiVersion)
             => $"{providerNamespace}/{apiVersion}/{TypeContainerName}".ToLowerInvariant();
 
-        public IEnumerable<TypeBase> LoadTypes(string providerNamespace, string apiVersion)
+        public string? GetContentAtPath(string? path)
         {
-            var streamName = GetTypeContainerResourceName(providerNamespace, apiVersion);
-            var fileStream = typeof(TypeLoader).Assembly.GetManifestResourceStream(streamName);
-
-            if (fileStream == null)
+            if (path is null ||
+                zipArchive.GetEntry(path) is not {} streamEntry)
             {
-                return Enumerable.Empty<TypeBase>();
+                return null;
             }
 
-            using (fileStream)
+            using (var fileStream = streamEntry.Open())
             using (var streamReader = new StreamReader(fileStream))
             {
-                var content = streamReader.ReadToEnd();
-
-                return TypeSerializer.Deserialize(content);
+                return streamReader.ReadToEnd();
             }
         }
 
         public ResourceType LoadResourceType(TypeLocation typeLocation)
         {
-            var fileStream = typeof(TypeLoader).Assembly.GetManifestResourceStream(typeLocation.RelativePath);
-            if (fileStream == null)
+            var content = GetContentAtPath(typeLocation.RelativePath);
+            if (content is null)
             {
                 throw new ArgumentException($"Unable to locate type \"{typeLocation.RelativePath}\" resource");
             }
 
-            using (fileStream)
-            using (var streamReader = new StreamReader(fileStream))
+            var types = TypeSerializer.Deserialize(content);
+
+            if (typeLocation.Index is not int intIndex ||
+                types[intIndex] is not ResourceType resourceType)
             {
-                var content = streamReader.ReadToEnd();
-
-                var types = TypeSerializer.Deserialize(content);
-                if (!(typeLocation.Index is int intIndex && types[intIndex] is ResourceType resourceType))
-                {
-                    throw new ArgumentException($"Unable to locate resource type at index {typeLocation.Index} in \"{typeLocation.RelativePath}\" resource");
-                }
-
-                return resourceType;
+                throw new ArgumentException($"Unable to locate resource type at index {typeLocation.Index} in \"{typeLocation.RelativePath}\" resource");
             }
+
+            return resourceType;
         }
 
         public TypeIndex GetIndexedTypes()
         {
-            var fileStream = typeof(TypeLoader).Assembly.GetManifestResourceStream(TypeIndexResourceName);
-            if (fileStream == null)
+            var content = GetContentAtPath(TypeIndexResourceName);
+            if (content is null)
             {
                 throw new ArgumentException("Unable to locate type index resource");
             }
 
-            using (fileStream)
-            using (var streamReader = new StreamReader(fileStream))
-            {
-                var content = streamReader.ReadToEnd();
-
-                return TypeIndexer.DeserializeIndex(content);
-            }
+            return TypeIndexer.DeserializeIndex(content);
         }
     }
 }
