d2e251041081237cc47936a78a20f885ebb14a47
Anthony Martin <antmarti@microsoft.com>
2021-05-21 10:52:49 -0400

    Add some comments

--- src/generator/cmd/generate.ts
+++ src/generator/cmd/generate.ts
@@ -79,7 +79,8 @@ executeSynchronous(async () => {
     const config = getConfig(basePath);
 
     try {
-      await buildConfiguration(readmePath, bicepReadmePath, config);
+      // autorest readme.bicep.md files are not checked in, so we must generate them before invoking autorest
+      await generateAutorestConfig(readmePath, bicepReadmePath, config);
       await generateSchema(logger, readmePath, outputDir, verbose, waitForDebugger);
 
       await copyRecursive(outputDir, `${outputBaseDir}/${basePath}`);
@@ -91,6 +92,7 @@ executeSynchronous(async () => {
 
     // clean up temp dir
     await rmdir(outputDir, { recursive: true });
+    // clean up autorest readme.bicep.md files
     await rm(bicepReadmePath, { force: true });
   }
 
@@ -98,23 +100,37 @@ executeSynchronous(async () => {
   await buildTypeIndex(defaultLogger, outputBaseDir);
 });
 
-async function buildConfiguration(readmePath: string, bicepReadmePath: string, config: GeneratorConfig) {
+async function generateAutorestConfig(readmePath: string, bicepReadmePath: string, config: GeneratorConfig) {
+  // We expect a path format convention of <provider>/(preview|stable)/<yyyy>-<mm>-<dd>(|-preview)/<filename>.json
+  // This information is used to generate individual tags in the generated autorest configuration
   const pathRegex = /^([^\/]+)\/[^\/]+\/(\d{4}-\d{2}-\d{2}(|-preview))\/.*\.json$/i;
+
   const readmeContents = await readFile(readmePath, { encoding: 'utf8' });
   const readmeMarkdown = markdown.parse(readmeContents);
 
   const inputFiles = new Set<string>(config.additionalFiles);
-  for (const codeBlock of markdown.iterate(readmeMarkdown.markDown)) {
-    if (codeBlock.type === 'code_block' && codeBlock?.info?.startsWith('yaml') && codeBlock.literal !== null) {
-      const yamlBlock = yaml.load(codeBlock.literal) as any;
-      if (yamlBlock) {
-        const inputFile = yamlBlock['input-file'];
-        if (typeof inputFile === 'string') {
-          inputFiles.add(inputFile.replace(/[\\\/]/g, '/'));
-        } else if (inputFile instanceof Array) {
-          for (const i of inputFile) {
-            inputFiles.add(i.replace(/[\\\/]/g, '/'));
-          }
+  // we need to look for all autorest configuration elements containing input files, and collect that list of files. These will look like (e.g.):
+  // ```yaml $(tag) == 'someTag'
+  // input-file:
+  // - path/to/file.json
+  // - path/to/other_file.json
+  // ```
+  for (const node of markdown.iterate(readmeMarkdown.markDown)) {
+    // We're only interested in yaml code blocks
+    if (node.type !== 'code_block' || !node.info || !node.literal ||
+      !node.info.trim().startsWith('yaml')) {
+      continue;
+    }
+    
+    const yamlData = yaml.load(node.literal);
+    if (typeof yamlData === 'object') {
+      // input-file may be a single string or an array of strings
+      const inputFile = yamlData['input-file'];
+      if (typeof inputFile === 'string') {
+        inputFiles.add(inputFile.replace(/[\\\/]/g, '/'));
+      } else if (inputFile instanceof Array) {
+        for (const i of inputFile) {
+          inputFiles.add(i.replace(/[\\\/]/g, '/'));
         }
       }
     }
@@ -124,6 +140,9 @@ async function buildConfiguration(readmePath: string, bicepReadmePath: string, c
   for (const file of inputFiles) {
     const match = pathRegex.exec(file);
     if (match) {
+      // Generate a unique tag. We can't process all of the different API versions in one autorest pass
+      // because there are constraints on naming uniqueness (e.g. naming of definitions), so we want to pass over
+      // each API version separately.
       const tagName = `${match[1].toLowerCase()}-${match[2].toLowerCase()}`;
       if (!filesByTag[tagName]) {
         filesByTag[tagName] = [];
@@ -136,14 +155,14 @@ async function buildConfiguration(readmePath: string, bicepReadmePath: string, c
   let generatedContent = `##Bicep
 
 ### Bicep multi-api
-\`\`\` yaml $(bicep) && $(multiapi)
+\`\`\`yaml $(bicep) && $(multiapi)
 ${yaml.dump({ 'batch': Object.keys(filesByTag).map(tag => ({ 'tag': tag })) }, { lineWidth: 1000 })}
 \`\`\`
 `;
 
   for (const tag of Object.keys(filesByTag)) {
     generatedContent += `### Tag: ${tag} and bicep
-\`\`\` yaml $(tag) == '${tag}' && $(bicep)
+\`\`\`yaml $(tag) == '${tag}' && $(bicep)
 ${yaml.dump({ 'input-file': filesByTag[tag] }, { lineWidth: 1000})}
 \`\`\`
 `;
@@ -158,7 +177,9 @@ async function generateSchema(logger: ILogger, readme: string, outputBaseDir: st
     '--bicep',
     `--output-folder=${outputBaseDir}`,
     `--multiapi`,
-    // this is necessary to avoid failures such as "ERROR: Semantic violation: Discriminator must be a required property." blocking type generation
+    // This is necessary to avoid failures such as "ERROR: Semantic violation: Discriminator must be a required property." blocking type generation.
+    // In an ideal world, we'd raise issues in https://github.com/Azure/azure-rest-api-specs and force RP teams to fix them, but this isn't very practical
+    // as new validations are added continuously, and there's often quite a lag before teams will fix them - we don't want to be blocked by this in generating types. 
     `--skip-semantics-validation`,
     readme,
   ];
