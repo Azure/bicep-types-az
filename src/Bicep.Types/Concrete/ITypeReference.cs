f429b606e4f48ec8b3bd425fcf8f587de6e730d4
Anthony Martin <38542602+anthony-c-martin@users.noreply.github.com>
2022-09-28 14:21:16 -0400

    Use Azure.Bicep.Types nuget package (#1123)

--- src/Bicep.Types/Concrete/ITypeReference.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-namespace Azure.Bicep.Types.Concrete
-{
-    public interface ITypeReference
-    {
-        TypeBase Type { get; }
-    }
-}
--- src/Bicep.Types/Concrete/TypeBase.cs
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-namespace Azure.Bicep.Types.Concrete
-{
-    public abstract class TypeBase
-    {
-    }
-}
