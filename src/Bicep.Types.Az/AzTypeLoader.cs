ee505afa4f6d93882dcced72f4109bae6c50e5c1
Anthony Martin <38542602+anthony-c-martin@users.noreply.github.com>
2022-10-14 14:09:36 +0000

    Preview changes for https://github.com/Azure/bicep-types/pull/27

---
+++ src/Bicep.Types.Az/AzTypeLoader.cs
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using Azure.Bicep.Types;
+using Azure.Bicep.Types.Concrete;
+
+namespace Azure.Bicep.Types.Az;
+
+public class AzTypeLoader : TypeLoader
+{
+    protected override Stream GetContentStreamAtPath(string path)
+    {
+        var fileStream = typeof(AzTypeLoader).Assembly.GetManifestResourceStream($"{path}.deflated");
+        if (fileStream is null)
+        {
+            throw new ArgumentException($"Unable to locate manifest resource at path {path}", nameof(path));
+        }
+
+        return new DeflateStream(fileStream, CompressionMode.Decompress);
+    }
+}
