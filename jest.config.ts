d4d3e15a80d71e297bd0a08c3b4ec8c49f4ab600
Ariel Silverman <asilverman@microsoft.com>
2023-10-02 21:25:54 -0700

    implement tests

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+module.exports = async () => {
+  return {
+    preset: 'ts-jest',
+    verbose: true,
+  };
+};
---
+++ scripts/github-actions/publish-az-types.js
@@ -0,0 +1,76 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+/**
+* @typedef Params
+* @property {typeof require} require the 'require' function from github actions
+* @property {string} registryFqdn the fully qualified domain name of the registry to publish to
+* @property {string} tags the tags to apply to the published package
+* @property {typeof import("@actions/core")} core 
+*
+* @param {Params} params - The arguments for publishing the package.
+*
+* @returns {void}
+*/
+function publishTypeProviderPackage({ require, registryFqdn, tags, core }) {
+    const fs = require('fs');
+    const tar = require('tar');
+    const { spawnSync } = require('child_process');
+    const path = require('path');
+
+    const azAcrRepoPath = 'bicep/providers/az';
+    const typeSourcesPath = path.join(process.cwd(), "generated");
+
+    const outDir = 'bin';
+    // Check and create the output directory if it doesn't exist
+    try {
+        fs.accessSync(outDir);
+    } catch {
+        fs.mkdirSync(outDir, { recursive: true });
+    }
+
+    core.info(`Creating tar+gz file from json files in folder: ${typeSourcesPath}`);
+    const tarFilePath = path.join(outDir, "types.tgz");
+    const files = fs.readdirSync(typeSourcesPath, { recursive: true }).filter(file => file.endsWith('.json'));
+    tar.create({
+        cwd: typeSourcesPath,
+        gzip: true,
+        portable: true,
+        file: tarFilePath,
+        sync: true,
+    }, files);
+
+    core.info(`Tar file created at ${tarFilePath}`);
+
+    core.info('Creating empty JSON blob config file for ORAS mediaType');
+    const configFilePath = path.join(outDir, 'config.json');
+    fs.writeFileSync(configFilePath, '{}');
+    core.info(`config.json file created at ${configFilePath}`);
+
+    core.info(`Publishing types package to ${registryFqdn}:${tags}`);
+    const artifactMediaType = 'application/vnd.ms.bicep.provider.artifact';
+    const configMediaType = 'application/vnd.ms.bicep.provider.config.v1+json';
+    const layerMediaType = 'application/vnd.ms.bicep.provider.layer.v1.tar+gzip';
+
+    const result = spawnSync('oras', [
+        'push',
+        `${registryFqdn}/${azAcrRepoPath}:${tags}`,
+        `--artifact-type`, artifactMediaType,
+        '--config', `${configFilePath}:${configMediaType}`,
+        `${tarFilePath}:${layerMediaType}`
+    ]);
+
+    if (result.status !== 0) {
+        core.setFailed(`Error publishing types package: ${result.stderr}`);
+        return
+    }
+    core.info(`Successfully published types package to ${registryFqdn}:${tags}`);
+}
+
+/**
+ * @module publish-az-types
+ */
+module.exports = { publishTypeProviderPackage };
+
+
+
