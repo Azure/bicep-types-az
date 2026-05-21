0b794cfb9514a5c54b87fb9d0ed62be341fe0d77
Mingzhe Jiang <71346160+jiangmingzhe@users.noreply.github.com>
2026-05-21 08:02:24 +0800

    fix: remove keyvault additionalFiles workaround (now covered by new readme in KeyVault/ subfolder) (#2736)
    Co-authored-by: Mingzhe Jiang <mingzhejiang@microsoft.com>

--- src/generator/src/config.ts
+++ src/generator/src/config.ts
@@ -10,14 +10,6 @@ const defaultConfig: GeneratorConfig = {
 }
 
 const config: Record<string, GeneratorConfig> = {
-  'keyvault': {
-    additionalFiles: [
-      'Microsoft.KeyVault/stable/2016-10-01/secrets.json',
-      'Microsoft.KeyVault/stable/2018-02-14/secrets.json',
-      'Microsoft.KeyVault/preview/2018-02-14-preview/secrets.json',
-      'Microsoft.KeyVault/stable/2019-09-01/secrets.json',
-    ],
-  }
 }
 
 export function getConfig(basePath: string): GeneratorConfig {
